85013b1e8c66e0b1d1a81dac10e12fb1b0beddd6
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-05-18 10:03:58 +0200

    push exo6 dom

---
+++ main.js
@@ -0,0 +1,24 @@
+// > Event Target
+
+// ### 1. Suit scrupuleusement les étapes ci-dessous
+// - Récupère la div en passant par son id
+// - Créer une function avec comme paramètre "e" (qui symbolise "event") 
+// - L'instruction de ta function est de faire un console.log de ton paramêtre e
+// - Créer un écouteur d'événement "click" sur ta div et observe ce que ton console log renvoi quand tu clique sur :
+//     - la div
+//     - le premier h1
+//     - le second h1
+// - Ouvre ce que tu récupères dans tes console.log et cherche la propriété target, observe ce qu'il contient
+
+// ### 2. Trouve le moyen d'afficher l'élément dans la console.log grace a la propriété target
+
+
+// ### 3. Change ton console.log par une instruction qui permet de mettre sur l'élément un border red et observe en cliquant sur les éléments de ta page ce qu'il se passe
+
+let exo1 = document.getElementById('content');
+
+function exo2(e) {
+    e.target.style.border = 'red solid 1px' ;
+}
+
+exo1.addEventListener('click', exo2)
